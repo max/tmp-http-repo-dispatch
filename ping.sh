59e6c4189dd120d6f50968f31b92f9719cecc7a7
Max Schoening <max@max.wtf>
2019-03-17 12:38:49 -0700

    Use auth header

--- ping.sh
+++ ping.sh
@@ -1,7 +1,8 @@
 #!/usr/bin/env sh
 
-curl -XPOST -u "max:$GITHUB_TOKEN" \
--H "Accept: application/vnd.github.everest-preview+json"  \
--H "Content-Type: application/json" \
-https://api.github.com/repos/max/tmp-http-repo-dispatch/dispatches \
---data '{"event_type": "ping.pong"}'
+curl -XPOST \
+  -H "Accept: application/vnd.github.everest-preview+json"  \
+  -H "Authorization: token $GITHUB_TOKEN" \
+  -H "Content-Type: application/json" \
+  https://api.github.com/repos/max/tmp-http-repo-dispatch/dispatches \
+  --data '{"event_type": "ping.pong"}'
